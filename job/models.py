4a5a41a8949842f3c3b23360e3337966b9506e76
braveheart10060 <braveheart10060@gmail.com>
2022-10-10 02:50:48 +0300

    add urls and views

--- job/models.py
+++ job/models.py
@@ -1,3 +1,25 @@
+from email.policy import default
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+JOB_TYPE =(
+    ('full time','full time'),
+    ('part time','part time'),
+)
+class job(models.Model):
+    title = models.CharField(max_length=100)
+    #location
+    job_type = models.CharField(max_length=15 , choices = JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField( auto_now=True)
+    Vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experiance = models.IntegerField(default=1)
+    category = models.ForeignKey('category',on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+class category(models.Model):
+    name = models.CharField(max_length=25)
+    def __str__(self):
+        return self.name
